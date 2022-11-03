a16061c14adefc1af6c2474115ada2c8ab5f2054
Daniel Jo <67842986+danielskjo@users.noreply.github.com>
2022-11-03 11:53:02 -0700

    Added resources and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from os import path
 
 from flask import Flask
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,12 +10,18 @@ DB_NAME = "university.db"
 
 def create_app():
     app = Flask(__name__)
+    api = Api(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .models import Course
 
     create_database(app)
+
+    from .resources import CoursesResource, CourseResource
+
+    api.add_resource(CoursesResource, '/courses')
+    api.add_resource(CourseResource, '/course/<string:department>/<int:number>')
 
     @app.route("/")
     def hello_world():
---
+++ app/resources.py
@@ -0,0 +1,87 @@
+from flask_restful import Resource, abort, fields, marshal_with, reqparse
+
+from app import db
+from .models import Course
+
+courses_post_args = reqparse.RequestParser()
+courses_post_args.add_argument("department", type=str, help="Course department", required=True)
+courses_post_args.add_argument("number", type=int, help="Course number", required=True)
+courses_post_args.add_argument("name", type=str, help="Course name", required=True)
+courses_post_args.add_argument("units", type=int, help="Number of units of course", required=True)
+courses_post_args.add_argument("desc", type=str, help="Course description")
+
+course_put_args = reqparse.RequestParser()
+course_put_args.add_argument("name", type=str, help="Course name")
+course_put_args.add_argument("units", type=int, help="Number of units of course")
+course_put_args.add_argument("desc", type=str, help="Course description")
+
+resource_fields = {
+    'department': fields.String,
+    'number': fields.Integer,
+    'name': fields.String,
+    'units': fields.Integer,
+    'desc': fields.String
+}
+
+
+class CoursesResource(Resource):
+    @marshal_with(resource_fields)
+    def get(self):
+        return Course.query.all(), 200
+
+    @marshal_with(resource_fields)
+    def post(self):
+        args = courses_post_args.parse_args()
+        result = Course.query.get((args['department'], args['number']))
+
+        if result:
+            abort(400, message="Course with this number in this department already exists")
+
+        course = Course(department=args['department'], number=args['number'], name=args['name'], units=args['units'],
+                        desc=args['desc'])
+        db.session.add(course)
+        db.session.commit()
+
+        return course, 201
+
+
+class CourseResource(Resource):
+    @marshal_with(resource_fields)
+    def get(self, department, number):
+        result = Course.query.get((department, number))
+
+        if not result:
+            abort(404, message="Course with this number in this department does not exist")
+
+        return result, 200
+
+    @marshal_with(resource_fields)
+    def put(self, department, number):
+        args = course_put_args.parse_args()
+        result = Course.query.get((department, number))
+
+        if not result:
+            abort(404, message="Course with this number in this department does not exist")
+
+        if args['name']:
+            result.name = args['name']
+        if args['units']:
+            result.units = args['units']
+        if args['desc']:
+            result.desc = args['desc']
+
+        db.session.commit()
+
+        return result, 200
+
+    @marshal_with(resource_fields)
+    def delete(self, department, number):
+        result = Course.query.get((department, number))
+
+        if not result:
+            abort(404, message="Course with this number in this department does not exist")
+
+        db.session.delete(result)
+        db.session.commit()
+
+        return '', 204
